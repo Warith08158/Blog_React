5ea1d0bfa13d4bc6a3aadc54012ab5ff8ff2ac67
solaudeen <solaudeenw@gmail.com>
2024-05-27 02:09:06 -0700

    completed menu nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,58 @@
 import React, { useEffect, useRef, useState } from "react";
 import Logo from "./Logo";
+import { Link, useLocation } from "react-router-dom";
+
+const links = [
+  {
+    to: "Dashboard",
+    path: "/protected-route/user-dashboard",
+  },
+
+  {
+    to: "Blog",
+    path: "/",
+  },
+
+  {
+    to: "Inbox",
+  },
+
+  {
+    to: "Users",
+  },
+
+  {
+    to: "Products",
+  },
+
+  {
+    to: "Sign In",
+    path: "/sign-in",
+  },
+
+  {
+    icon: (
+      <svg
+        className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
+        <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
+        <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
+      </svg>
+    ),
+    to: "Sign Up",
+    path: "/sign-up",
+  },
+];
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const [loading, setLoading] = useState(true);
-
+  const location = useLocation().pathname;
   const handleImageLoad = () => {
     setLoading(false);
   };
@@ -71,39 +119,23 @@ const Navbar = () => {
         <div className="sm:hidden sticky top-0" id="navbar-hamburger">
           <div className="px-3 py-3 lg:px-5 lg:pl-3 max-w-[1280px] mx-auto">
             <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 rounded dark:bg-blue-600 text-white bg-primary-600 hover:bg-primary-700"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  Contact
-                </a>
-              </li>
+              {links.map((link) => (
+                <li onClick={() => setOpenMenu(false)} key={link.to}>
+                  <Link
+                    onClick={() => setOpenMenu(false)}
+                    to={link.path}
+                    href="#"
+                    className={`${
+                      location === link.path
+                        ? "bg-primary-600 text-white "
+                        : "hover:bg-gray-100 text-gray-900"
+                    } block py-2 px-3 rounded`}
+                    aria-current="page"
+                  >
+                    {link.to}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
