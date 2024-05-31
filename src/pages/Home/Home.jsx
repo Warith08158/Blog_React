653009224e9ef9141c97956ef6a7d140f77121d1
solaudeen <solaudeenw@gmail.com>
2024-05-31 05:53:00 -0700

    added skeleton loading to userDashboard component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import FormInput from "../../components/FormInput";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="px-3 sm:px-10 py-3">
+      <h4 className="text-2xl font-bold racking-wide text-gray-950 dark:text-white">
+        Read Latest Blog
+      </h4>
+      <p className="mt-10">Filter by</p>
+      <div className="mt-2">
+        <FormInput />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
