b5a841fd771c1eb8cc6c66e0af71363f370b1feb
solaudeen <solaudeenw@gmail.com>
2024-06-02 14:23:59 -0700

    added images to firebase

--- src/components/UserImages.jsx
+++ src/components/UserImages.jsx
@@ -1,17 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CiCamera } from "react-icons/ci";
+import { auth, db } from "../../Firebase";
+import {
+  deleteField,
+  doc,
+  getDoc,
+  updateDoc,
+  onSnapshot,
+} from "firebase/firestore";
 import {
   getStorage,
   ref,
   uploadBytesResumable,
   getDownloadURL,
+  deleteObject,
 } from "firebase/storage";
 import { v4 as v4uuid } from "uuid";
-import { auth } from "../../Firebase";
+
 const UserImages = ({ name, email, posts, comments }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isLoadingImage, setIsLoadingImage] = useState(true);
   const [loadingPercentage, setLoadingPercentage] = useState(null);
+  const [images, setImages] = useState({ avatar: null, coverPhoto: null });
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "users", auth.currentUser.uid), (doc) => {
+      // console.log(doc.data().avatar, doc.data().cover + "-" + photo);
+      console.log(doc.data());
+      setImages({
+        coverPhoto: doc.data().coverPhoto ? doc.data().coverPhoto : null,
+        avatar: doc.data().avatar ? doc.data().avatar : null,
+      });
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
 
   const onLoadCoverImage = () => {
     setIsLoading(false);
@@ -21,36 +46,110 @@ const UserImages = ({ name, email, posts, comments }) => {
   };
 
   const changePhoto = (e) => {
+    const image = e.target.id;
     const file = e.target.files[0];
+    const fileName = `${auth.currentUser.uid}-${v4uuid()}-${
+      file.name
+    } - ${image}`;
+
+    //current user uid
+    const userUID = auth.currentUser.uid;
+
+    //if an image is not selected
     if (!e.target.files[0]) {
       return;
     }
-    const fileName = `${auth.currentUser.uid}-${v4uuid()}-${file.name} - ${
-      e.target.id
-    }`;
 
+    //continue if an image is selected
+    const docRef = doc(db, "users", userUID);
     const storage = getStorage();
-    const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        setLoadingPercentage(progress);
-      },
-      (error) => {
-        console.log(error);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-        });
-        setLoadingPercentage(null);
+    //check if an image is available in firestore
+    async function ImageExists() {
+      const docSnap = await getDoc(docRef);
+      if (image in docSnap.data()) {
+        return true;
+      } else {
+        return false;
       }
-    );
+    }
+    ImageExists().then((imageIsAvailable) => {
+      imageIsAvailable ? updateImage() : addImage();
+    });
+
+    //if image is available in firestore
+    async function updateImage() {
+      try {
+        //delete image from storage
+        const docSnap = await getDoc(docRef);
+        const desertRef = ref(storage, docSnap.data()[image]);
+        await deleteObject(desertRef);
+
+        //delete link from firestore
+        const imageRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(imageRef, {
+          [image]: deleteField(),
+        });
+
+        //add new image to storage
+        addImage();
+      } catch (error) {
+        console.log(error);
+      }
+
+      //return
+      return;
+    }
+
+    //if image is not available in firestore
+    function addImage() {
+      //add new image to storage
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setLoadingPercentage(progress);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        async () => {
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+
+          //call add new link to firestore function
+          addLinkToFirestore(downloadURL);
+        }
+      );
+
+      //return
+      return;
+    }
+
+    //add new link to firestore
+    async function addLinkToFirestore(URL) {
+      setLoadingPercentage(null);
+      const updateImageURL = {
+        [image]: URL,
+      };
+
+      try {
+        await updateDoc(docRef, updateImageURL);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
@@ -67,17 +166,39 @@ const UserImages = ({ name, email, posts, comments }) => {
       </div>
 
       <div>
-        <img
-          onLoad={onLoadCoverImage}
-          className={`object-cover w-full max-w-full rounded-lg h-[330px] ${
-            isLoading ? "hidden" : ""
-          }`}
-          src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-          alt="cover image"
-        />
+        {images.coverPhoto ? (
+          <img
+            onLoad={onLoadCoverImage}
+            className={`object-cover w-full max-w-full rounded-lg h-[330px] ${
+              isLoading ? "hidden" : ""
+            }`}
+            src={images.coverPhoto}
+            alt="cover image"
+          />
+        ) : (
+          <div
+            className={`object-cover w-full max-w-full rounded-lg h-[330px] ${
+              isLoading ? "hidden" : ""
+            }`}
+          >
+            <svg
+              className=" text-gray-300 w-full h-full object-contain"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+        )}
+
         {!loadingPercentage && (
           <label
-            htmlFor="cover-photo"
+            htmlFor="coverPhoto"
             className="p-1 absolute bottom-4 right-4 rounded-full bg-gray-100 z-10 cursor-pointer"
           >
             <CiCamera className="text-red text-xl text-gray-600" />
@@ -87,7 +208,7 @@ const UserImages = ({ name, email, posts, comments }) => {
           className="hidden"
           type="file"
           name="coverPhoto"
-          id="cover-photo"
+          id="coverPhoto"
           accept="image/jpeg, image/png"
           onChange={changePhoto}
         />
@@ -98,14 +219,36 @@ const UserImages = ({ name, email, posts, comments }) => {
       <div className="absolute inset-0 bg-gradient-to-r rounded-lg from-black via-transparent/70 to-black opacity-90"></div>
       <div className="absolute top-6 left-0 right-0 flex items-center flex-col gap-6 justify-center">
         <div className="relative border-2 border-white rounded-full">
-          <img
-            onLoad={onLoadImage}
-            className={`w-28 h-28 rounded-full ${
-              isLoadingImage ? "hidden" : ""
-            }`}
-            src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-            alt="image"
-          />
+          {images.avatar ? (
+            <img
+              onLoad={onLoadImage}
+              className={`w-28 h-28 rounded-full object-cover ${
+                isLoadingImage ? "hidden" : ""
+              }`}
+              src={images.avatar}
+              alt="image"
+            />
+          ) : (
+            <div
+              className={`w-28 h-28 rounded-full ${
+                isLoadingImage ? "hidden" : ""
+              }`}
+            >
+              <svg
+                className=" text-gray-200 w-full h-full object-contain"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </div>
+          )}
+
           {!loadingPercentage && (
             <label
               htmlFor="avatar"
